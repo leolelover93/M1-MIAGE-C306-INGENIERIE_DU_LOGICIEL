d7b822d77e7795e32b7b2abd82689956b4059b04
leolelover93 <leolelover93@gmail.com>
2020-05-21 12:18:05 +0100

    add folder activite2

---
+++ activite2/Exercice03/src/main/java/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Classe <strong>GrilleImpl</strong> representant
+ * une implementation d'une grille de sudoku.
+ *
+ * <p>
+ * Il s'agit d'une classe implementee dans le
+ * cadre de la formation E-MIAGE, et plus
+ * precisemment pour le module
+ * <strong>C-306</strong> intutle
+ * <strong>Ingenierie du logiciel (ISI_05).</strong>
+ * </p>
+ *
+ * @since Avril 2020
+ * @author LEUMASSI FANSI Jean-Leopold
+ * @version 1.0
+ **/
+package main.java;
